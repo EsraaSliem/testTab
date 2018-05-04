071f7e6c428bbef034df2b94a6b27cd599922b4f
EsraaSliem <esraasliem8@gmail.com>
2018-05-04 21:17:33 +0200

    updated

--- app/src/main/java/com/example/hp/myapplication/One.java
+++ app/src/main/java/com/example/hp/myapplication/One.java
@@ -1,10 +1,18 @@
 package com.example.hp.myapplication;
 
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
@@ -12,6 +20,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import static android.content.Context.LOCATION_SERVICE;
 
@@ -27,7 +36,6 @@ public class One extends Fragment {
     }
 
 
-
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,28 +43,84 @@ public class One extends Fragment {
     }
 
     Button btn;
+    TextView txt;
+    private LocationManager locationManager;
+    private LocationListener locationListener;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-      View v= inflater.inflate(R.layout.fragment_one, container, false);
+        View v = inflater.inflate(R.layout.fragment_one, container, false);
+        btn = (Button) v.findViewById(R.id.one);
+        txt = (TextView) v.findViewById(R.id.txtONe);
+
+        locationManager = (LocationManager) getContext().getSystemService(LOCATION_SERVICE);
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                txt.append("\n" + location.getLatitude() + "," + location.getLongitude());
+            }
+
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String s) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String s) {
+                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(intent);
+            }
+        };
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(new String[]{
+                        Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION,
+                        Manifest.permission.INTERNET
+                }, 10);
+
+
+            }
+        } else {
+            configrationButton();
+
+        }
 
 
 
-       btn =(Button)v.findViewById(R.id.one);
+        return v;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case 10:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                    configrationButton();
+                return;
+        }
+    }
 
 
+    private void configrationButton() {
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Fragment done = new Two();
+
+                locationManager.requestLocationUpdates("gps", 5000, 0, locationListener);
+             /*   Fragment done = new Two();
                 FragmentManager ft = getFragmentManager();
-                ft.beginTransaction().replace(R.id.frameone, done).commit();
+                ft.beginTransaction().replace(R.id.frameone, done).commit();*/
 
             }
         });
-      return v;
+
     }
 
 }
